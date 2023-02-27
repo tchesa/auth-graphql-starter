205a1fc978cf8a3faf56f2784bcc350168fce03f
tchesa <cesarolimpio@gmail.com>
2023-02-27 20:13:35 -0300

    query for logged user

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,11 +1,15 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require("./user_type");
+const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    bla: {
-      type: GraphQLID,
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      },
     },
   },
 });
