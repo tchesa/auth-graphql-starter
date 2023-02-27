9062f61e5c9d864376b69843a26ac69952da02c4
tchesa <cesarolimpio@gmail.com>
2023-02-27 13:21:42 -0300

    create user type and signup mutation

---
+++ server/schema/mutations.js
@@ -0,0 +1,26 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
+const UserType = require("./types/user_type");
+const AuthService = require("../services/auth");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: {
+          type: GraphQLString,
+        },
+        password: {
+          type: GraphQLString,
+        },
+      },
+      resolve(_, { email, password }, context) {
+        return AuthService.signup({ email, password, req: context });
+      },
+    },
+  },
+});
+
+module.exports = mutation;
